8e8114414d71056b127506ee72d5cbcf4128313b
Mario Fink <mario.fink@goodyear.com>
2019-09-16 16:42:57 +0200

    render all available cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Word from "components/word/Word";
+import AllCards from "cards.json";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Word>OPA</Word>
+      <Word>
+        {Object.keys(AllCards)
+          .join("")
+          .toUpperCase()}
+      </Word>
     </div>
   );
 }
