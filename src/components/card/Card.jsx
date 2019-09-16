bb170b85d736f04abeef7b970838c39efe8022d8
Mario Fink <mario.fink@goodyear.com>
2019-09-16 15:02:34 +0200

    add some basic layout

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import styles from "./Card.module.css";
 
 export default props => {
   const image = require(`images/${props.image}`);
   return (
-    <div>
-      <div>
+    <div className={styles.card}>
+      <div className={styles.image}>
         <img src={image} alt="Card" />
       </div>
-      <div>{props.sound}</div>
+      <div className={styles.sound}>{props.sound}</div>
     </div>
   );
 };
