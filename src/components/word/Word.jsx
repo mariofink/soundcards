53f4d7aefbd2bdd3e809d637d499f4d9057b99de
Mario Fink <mario.fink@goodyear.com>
2019-09-24 21:51:15 +0200

    prevent errors for non-existing cards

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -3,16 +3,27 @@ import Card from "components/card/Card";
 import AllCards from "cards.json";
 import styles from "./Word.module.css";
 
+function renderCard(cardKey, i) {
+  const card = AllCards[cardKey];
+  if (typeof card === "undefined") {
+    console.warn("no card found for", cardKey)
+    return;
+  }
+  return (
+    <Card
+      image={AllCards[cardKey].image}
+      sound={AllCards[cardKey].sound}
+      key={cardKey + i}
+    ></Card>
+  )
+}
+
 export default props => {
   const word = props.children.split("");
   return (
     <div className={styles.word}>
       {word.map((cardKey, i) => (
-        <Card
-          image={AllCards[cardKey].image}
-          sound={AllCards[cardKey].sound}
-          key={cardKey + i}
-        ></Card>
+        renderCard(cardKey, i)
       ))}
     </div>
   );
