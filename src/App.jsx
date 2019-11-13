a561057a53d61bca5c2b8937c874930941b9d7ff
Mario Fink <mario.fink@goodyear.com>
2019-11-13 22:38:49 +0100

    add random word

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import Word from "components/word/Word";
 import allWords from "./words.json";
 import "./App.css";
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function App() {
   const [index, setIndex] = useState(0);
 
@@ -22,10 +35,15 @@ function App() {
     }
   }
 
+  function randomWord() {
+    setIndex(getRandomInt(0, allWords.length - 1));
+  }
+
   return (
     <div className="App">
       <Word sounds={allWords[index].sounds} />
       <button onClick={previousWord}>Vorheriges Wort</button>
+      <button onClick={randomWord}>ZUFALL!</button>
       <button onClick={nextWord}>Nächstes Wort</button>
     </div>
   );
